8a237798b9e8a7b21f65e4d001a9976a7fe55c73
Matthijs86 <boersmamatthijs@gmail.com>
2026-09-14 14:42:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // SERVICE WORKER
 // ======================================
 
-const CACHE_NAME = "anker-v48";
+const CACHE_NAME = "anker-v49";
 
 const APP_BESTANDEN = [
     "./",
@@ -155,23 +155,6 @@ self.addEventListener(
 // ======================================
 // FETCH
 // ======================================
-self.addEventListener(
-    "push",
-    (event) => {
-
-        event.waitUntil(
-            self.registration.showNotification(
-                "Anker push test",
-                {
-                    body: "Als je dit ziet, werkt mobiele push naar Anker.",
-                    icon: "./anker-icon-192x192.png",
-                    badge: "./anker-icon-192x192.png"
-                }
-            )
-        );
-
-    }
-);
 self.addEventListener(
     "fetch",
     (event) => {

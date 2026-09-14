a153db208bb39a0695728c2d2ed5bfba8b072529
Matthijs86 <boersmamatthijs@gmail.com>
2026-09-14 14:54:01 +0200

    Push test handler teruggezet

--- sw.js
+++ sw.js
@@ -155,6 +155,18 @@ self.addEventListener(
 // ======================================
 // FETCH
 // ======================================
+self.addEventListener("push", (event) => {
+    event.waitUntil(
+        self.registration.showNotification(
+            "Anker push test",
+            {
+                body: "Als je dit ziet, werkt mobiele push naar Anker.",
+                icon: "./anker-icon-192x192.png",
+                badge: "./anker-icon-192x192.png"
+            }
+        )
+    );
+});
 self.addEventListener(
     "fetch",
     (event) => {
